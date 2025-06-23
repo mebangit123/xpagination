3855a46d0b8368b422f65383b01ee1ae96a15159
unknown <mebannongrum333@gmail.com>
2025-06-23 10:59:28 +0530

    first commit

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+function Pagination() {
+  
+  return (
+    <div>
+      Hello world......
+    </div>
+  )
+}
+
+export default Pagination
