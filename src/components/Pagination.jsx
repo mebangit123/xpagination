97386acdb439e9a6f8b807340c66e898f33cfa50
unknown <mebannongrum333@gmail.com>
2025-06-25 15:00:50 +0530

    implement pagination functionality

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,16 +2,69 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import style from './Pagination.module.css'
 
+function PaginationPage({ updatePage, currentPage, totalPages }) {
+
+    const handlePrev = () => {
+        if(currentPage > 1){
+            updatePage(prev => prev - 1)
+        }
+    }
+
+    const handleNext = () => {
+        if(totalPages != currentPage){
+            updatePage(prev => prev + 1)
+        }
+    }
+
+    return (
+        <div className={style.paginationWrapper}>
+            <button onClick={handlePrev} disabled={currentPage == 1}> 
+              Previous               
+            </button>
+
+            <p className={style.pageCount}>{currentPage}</p>
+
+            <button onClick={handleNext} disabled={totalPages == currentPage}>                
+             Next
+            </button>
+        </div>
+    )
+}
+
 function Pagination() {
   const [employeeList, setEmployeeList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentEmployeeList, setCurrentEmployeeList] = useState([]);
+  const maxRecords = 10;
+  const [totalPages, setTotalPages] = useState(0)
+
   useEffect(() => {
     try {
       const response = axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
       response.then(data => setEmployeeList(data.data));
     } catch (error) {
       console.log(error);
+      alert('failed to fetch data');
     }    
   },[]);
+
+  useEffect(() => {
+
+        const startIndex = (currentPage - 1) * maxRecords
+        const endIndex = Math.min(currentPage * maxRecords, employeeList.length)
+
+        setCurrentEmployeeList([...employeeList].slice(startIndex, endIndex))
+        setTotalPages(Math.ceil(employeeList.length / maxRecords))
+
+    }, [currentPage, employeeList]);
+
+  useEffect(() => {
+
+      if(totalPages < currentPage && currentPage > 1){
+          setCurrentPage(prev => prev - 1)
+      }
+
+  }, [totalPages])
 
   return (
     <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
@@ -28,7 +81,7 @@ function Pagination() {
           </thead>  
           <tbody className={style.tblbody}>
             {
-            employeeList.map((emp) => (
+            currentEmployeeList.map((emp) => (
               <tr>
                 <td>{emp.id}</td>
                 <td>{emp.name}</td>
@@ -40,6 +93,7 @@ function Pagination() {
           </tbody>                  
         </table>
       </div>
+      {totalPages > 1 && (<PaginationPage updatePage={setCurrentPage} currentPage={currentPage} totalPages={totalPages} />)}
     </div>
   )
 }
